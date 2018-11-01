19c14b07d0ac47b22030d76a86c4952089462b43
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-01 23:33:38 +0100

    Added SpringBoot project structure to deep dive in Dependency Injection with the Spring Framework

---
+++ spring5didemo/src/main/java/com/zawadal/spring5didemo/Spring5didemoApplication.java
@@ -0,0 +1,12 @@
+package com.zawadal.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5didemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5didemoApplication.class, args);
+	}
+}
