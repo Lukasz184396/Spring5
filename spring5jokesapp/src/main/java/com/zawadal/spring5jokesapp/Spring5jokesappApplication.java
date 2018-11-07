8b748f3a9ad2b4ca6a857dd7b3938049c56fef83
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-07 10:57:56 +0100

    Added Spring XML Configuration

--- spring5jokesapp/src/main/java/com/zawadal/spring5jokesapp/Spring5jokesappApplication.java
+++ spring5jokesapp/src/main/java/com/zawadal/spring5jokesapp/Spring5jokesappApplication.java
@@ -2,8 +2,12 @@ package com.zawadal.spring5jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5jokesappApplication {
 
 	public static void main(String[] args) {
