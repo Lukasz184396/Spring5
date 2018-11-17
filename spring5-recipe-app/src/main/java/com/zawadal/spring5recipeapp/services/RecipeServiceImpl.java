e1659bcb2884a71783a8e0a3d38636aac13b2f26
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-17 19:33:04 +0100

    Added loger in RecipeServiceImpl.java

--- spring5-recipe-app/src/main/java/com/zawadal/spring5recipeapp/services/RecipeServiceImpl.java
+++ spring5-recipe-app/src/main/java/com/zawadal/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.zawadal.spring5recipeapp.services;
 
 import com.zawadal.spring5recipeapp.domain.Recipe;
 import com.zawadal.spring5recipeapp.repositiories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -15,6 +17,8 @@ public class RecipeServiceImpl implements RecipeService {
     }
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the service");
+        
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
