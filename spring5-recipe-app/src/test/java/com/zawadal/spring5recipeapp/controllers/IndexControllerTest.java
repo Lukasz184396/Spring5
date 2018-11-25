d7f5f86504e10e2cee48c9df4384efe146148936
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-25 15:29:12 +0100

    Added unit test with SpringMockMVC

--- spring5-recipe-app/src/test/java/com/zawadal/spring5recipeapp/controllers/IndexControllerTest.java
+++ spring5-recipe-app/src/test/java/com/zawadal/spring5recipeapp/controllers/IndexControllerTest.java
@@ -2,22 +2,24 @@ package com.zawadal.spring5recipeapp.controllers;
 
 import com.zawadal.spring5recipeapp.domain.Recipe;
 import com.zawadal.spring5recipeapp.services.RecipeService;
-import com.zawadal.spring5recipeapp.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
-
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anySet;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -35,6 +37,15 @@ public class IndexControllerTest {
         indexController = new IndexController(recipeService);
     }
 
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
 
     @Test
     public void getIndexPage() throws Exception {
